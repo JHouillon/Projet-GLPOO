9ab33bb63882e3f210588d6ea15130b525d69578
Pierre <pfrin44@gmail.com>
2015-04-02 23:30:49 +0200

    Petit test :)

--- guhy/src/main/java/Jeu/Plateau.java
+++ guhy/src/main/java/Jeu/Plateau.java
@@ -1,8 +1,44 @@
 package Jeu;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class Plateau {
 
+	private static void ConfigurationFenetre() {
+		JFrame fenetre = new JFrame();
+		
+		Menu menu = new Menu();
+		
+		JPanel pan = new JPanel();
+		JPanel pan1 = new JPanel();
+		
+		JTextField text = new JTextField("Maxime a un petit zizi !");
+		JTextField text1 = new JTextField("Daniel à la sida !");
+		
+		fenetre.setSize(1024, 720);
+		fenetre.setTitle("Eternity");		
+		
+		pan.setBackground(Color.RED);
+		pan.add(text);
+		
+		pan1.setBackground(Color.ORANGE);
+		pan1.add(text1);
+		
+		fenetre.setJMenuBar(menu);
+		fenetre.getContentPane().add(pan, BorderLayout.EAST);
+		fenetre.getContentPane().add(pan1, BorderLayout.WEST);
+		fenetre.setLayout(new GridLayout(1,2));
+		fenetre.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		ConfigurationFenetre();
+	}
 }
 
-
-//coucocou test commit 
