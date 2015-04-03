93647acfc0eca0f871d7f055f76d4fb30ed47728
frin <frin@I72209.etd-p.esiea.fr>
2015-04-03 10:16:39 +0200

    ajout de la classe Piece, réorganisation des packages

--- guhy/src/main/java/Face/Face.java
+++ guhy/src/main/java/Face/Face.java
@@ -1,4 +1,4 @@
-package Pieces;
+package Face;
 
 public class Face {
 	
---
+++ guhy/src/main/java/Piece/Piece.java
@@ -0,0 +1,14 @@
+package Piece;
+
+public class Piece {
+	
+	private int id;
+	private int H;
+	private int D;
+	private int B;
+	private int G;
+	
+	
+	
+
+}
